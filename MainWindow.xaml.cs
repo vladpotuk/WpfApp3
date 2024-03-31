0ea13af3ece9a4875c947274df4accb20719692f
vladpotuk <vladpotuk01@gmail.com>
2024-03-31 22:31:30 +0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+
+namespace CalculatorApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Number_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                currentTextBox.Text += button.Content.ToString();
+            }
+        }
+
+        private void Operation_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button != null)
+            {
+                currentTextBox.Text += button.Content.ToString();
+            }
+        }
+
+        private void CE_Click(object sender, RoutedEventArgs e)
+        {
+            currentTextBox.Text = "";
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            currentTextBox.Text = "";
+        }
+
+        private void Backspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentTextBox.Text.Length > 0)
+            {
+                currentTextBox.Text = currentTextBox.Text.Substring(0, currentTextBox.Text.Length - 1);
+            }
+        }
+
+        private void Equal_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result = new System.Data.DataTable().Compute(currentTextBox.Text, null);
+                currentTextBox.Text = result.ToString();
+            }
+            catch (Exception ex)
+            {
+                currentTextBox.Text = "Error";
+            }
+        }
+    }
+}
